d4dd25c23febbfe12075dc353674476cbab43424
Jonne Haß <me@jhass.eu>
2016-08-06 10:54:19 +0200

    Add child-src CSP policy, as frame-src is deprecated

--- keeweb/CHANGELOG.md
+++ keeweb/CHANGELOG.md
@@ -1,4 +1,5 @@
 nextcloud-keeweb (0.2.0)
+* Add child-src to CSP (frame-src is deprecated)
 * Fix cache manifest
 
 nextcloud-keeweb (0.1.0)
--- keeweb/controller/pagecontroller.php
+++ keeweb/controller/pagecontroller.php
@@ -37,6 +37,7 @@ class PageController extends Controller {
 		// Override default CSP
 		$csp = new ContentSecurityPolicy();
 		$csp->addAllowedFrameDomain("'self'");
+		$csp->addAllowedChildSrcDomain("'self'");
 		$response->setContentSecurityPolicy($csp);
 		return $response;
 	}
