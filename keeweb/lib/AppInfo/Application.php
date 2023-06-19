b4490c387367ca55f5ac4b6e1ffd5e9bb77d0cb9
Bernd Stellwag <burned@zerties.org>
2023-06-19 18:49:32 +0200

    migrate navigation entry from old app.php concept to info.xml

--- keeweb/lib/AppInfo/Application.php
+++ keeweb/lib/AppInfo/Application.php
@@ -30,33 +30,6 @@ class Application extends App {
     }
 }
 
-$app = \OC::$server->query(Application::class);
-$container = $app->getContainer();
-
-$container->query('OCP\INavigationManager')->add(function () use ($container) {
-	$urlGenerator = $container->query('OCP\IURLGenerator');
-	$l10n = $container->query('OCP\IL10N');
-	return [
-		// the string under which your app will be referenced in Nextcloud
-		'id' => 'keeweb',
-
-		// sorting weight for the navigation. The higher the number, the higher
-		// will it be listed in the navigation
-		'order' => 2,
-
-		// the route that will be shown on startup
-		'href' => $urlGenerator->linkToRoute('keeweb.page.index'),
-
-		// the icon that will be shown in the navigation
-		// this file needs to exist in img/
-		'icon' => $urlGenerator->imagePath('keeweb', 'app.svg'),
-
-		// the title of your application. This will be used in the
-		// navigation or on the settings page of your app
-		'name' => $l10n->t('Keeweb'),
-	];
-});
-
 // Script for registering file actions
 $eventDispatcher = \OC::$server->getEventDispatcher();
 $eventDispatcher->addListener(
