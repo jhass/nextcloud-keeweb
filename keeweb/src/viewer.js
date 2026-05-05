8fd9b2db8866b0ac3dda49cf52890ba03182a236
Mohamed-Ismail MEJRI <imejri@hotmail.com>
2026-05-05 15:54:51 +0200

    Add support for Nextcloud 33 (Update actionFile API in viewer)

--- keeweb/src/viewer.js
+++ keeweb/src/viewer.js
@@ -1,22 +1,28 @@
-import {registerFileAction, FileAction, Permission, DefaultType} from '@nextcloud/files'
+import {registerFileAction, Permission, DefaultType} from '@nextcloud/files'
 import {generateUrl} from '@nextcloud/router'
 
-const fileAction = new FileAction({
+const fileAction = {
     id: 'keeweb',
     order: 1,
     default: DefaultType.DEFAULT,
     iconSvgInline(){
         return ""
     },
-    displayName() {
-        return t('keeweb', 'Open With Keeweb')
+    displayName: () => t('keeweb', 'Open With Keeweb'),
+    enabled: ({ nodes }) => {
+        return nodes.filter((node) => (node.permissions & Permission.READ !== 0 && node.mime === "application/x-kdbx")).length > 0
     },
-    enabled(nodes) {
-        return nodes.filter((node) => (node.permissions & Permission.READ !== 0 && node.mime ==="application/x-kdbx")).length > 0
+    exec: async ({ nodes }) => {
+        // Handle the first selected node with .kdbx extension
+        const node = nodes[0]
+        if (node && node.mime === "application/x-kdbx") {
+            // Build the URL with proper encoding
+            const encodedPath = encodeURIComponent(node.path)
+            const url = generateUrl('/apps/keeweb/?open=' + encodedPath)
+            // Use window.location.href for NC33+
+            window.location.href = url
+        }
     },
-    async exec(node, view, dir) {
-        OC.redirect(generateUrl('/apps/keeweb/?open={path}', { path: node.path }))
-    },
-})
+}
 
 registerFileAction(fileAction)
